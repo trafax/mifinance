678c0aa0f5a4d6c0d5dd1a58f2d1a6fe306e7d61
S van Spelden <sven@vanspelden.nl>
2020-06-04 11:53:49 +0200

    Route aanpassing

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\LoginController@showLoginForm');
 
-Auth::routes();
+Auth::routes(['register' => FALSE]);
 
 Route::get('/home', 'HomeController@index')->name('home');
